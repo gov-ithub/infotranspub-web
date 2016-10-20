0a5c27faffc0f96203aaebf04efa1879a288c00e
Gabriel Marchidan <gabriel.marchidan@feelit.iasi.net>
2016-10-20 10:41:11 +0300

    Optimized references and code. Added Responsive

---
+++ webapp/js/index.js
@@ -0,0 +1,69 @@
+
+var selectStart = "De la";
+var selectStop = "Până la";
+
+
+$( document ).ready(function() { 
+	for(var i =0;i<stationData.stationList.length;i++)
+	{
+		$("#option-group-from").append('<option value="'+stationData.stationList[i].stationId+'">'+stationData.stationList[i].name+'</option>');	
+		$("#option-group-to").append('<option value="'+stationData.stationList[i].stationId+'">'+stationData.stationList[i].name+'</option>');
+	}
+	
+});
+
+
+$( ".select-from" ).select2( { placeholder: selectStart } )
+$( ".select-to" ).select2( { placeholder: selectStop } )
+
+
+$('#searchBtn').on('click', function (e) {
+	var fromId = $("#select-search-from").select2("val");
+	var toId = $("#select-search-to").select2("val");
+
+	if(toId != "" && fromId !="" && fromId != toId)
+	{
+		window.location.href = "results.html?toId="+toId+"&fromId="+fromId;
+	}
+
+})
+
+// @see https://github.com/ivaynberg/select2/commit/6661e3
+function repoFormatResult( repo ) {
+	var markup = "<div class='select2-result-repository clearfix'>" +
+		"<div class='select2-result-repository__avatar'><img src='" + repo.owner.avatar_url + "' /></div>" +
+		"<div class='select2-result-repository__meta'>" +
+			"<div class='select2-result-repository__title'>" + repo.full_name + "</div>";
+	if ( repo.description ) {
+		markup += "<div class='select2-result-repository__description'>" + repo.description + "</div>";
+	}
+	markup += "<div class='select2-result-repository__statistics'>" + 
+				"<div class='select2-result-repository__forks'><span class='glyphicon glyphicon-flash'></span> " + repo.forks_count + " Forks</div>" +
+				"<div class='select2-result-repository__stargazers'><span class='glyphicon glyphicon-star'></span> " + repo.stargazers_count + " Stars</div>" +
+				"<div class='select2-result-repository__watchers'><span class='glyphicon glyphicon-eye-open'></span> " + repo.watchers_count + " Watchers</div>" +
+			"</div>" +
+		"</div></div>";
+	return markup;
+}
+
+			
+var select2OpenEventName = "select2-open";
+$( ":checkbox" ).on( "click", function() {
+	$( this ).parent().nextAll( "select" ).select2( "enable", this.checked );
+});
+			
+
+$( ".select2, .select2-multiple, .select2-allow-clear, .select2-remote" ).on( select2OpenEventName, function() {
+	if ( $( this ).parents( "[class*='has-']" ).length ) {
+		var classNames = $( this ).parents( "[class*='has-']" )[ 0 ].className.split( /\s+/ );
+		for ( var i = 0; i < classNames.length; ++i ) {
+			if ( classNames[ i ].match( "has-" ) ) {
+				$( "#select2-drop" ).addClass( classNames[ i ] );
+			}
+		}
+	}
+});
+
+
+
+
--- webapp/js/results.js
+++ webapp/js/results.js
@@ -8,12 +8,13 @@ function getParameterByName(name,url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 var locationStub = '<li class="[color]"><div class="where">[StationName]</div><p class="description">[Duration]</p></li>';
+var legList = [];
 
 $( document ).ready(function() {
     // console.log( stationData.stationList[0].name );
     var toId = parseInt(getParameterByName("toId"));
     var fromId = parseInt(getParameterByName("fromId"));
-    var legList = [];
+    legList = [];
     if(fromId < toId)
     {
 	    for(var i =fromId-1;i<toId-1;i++)
@@ -35,11 +36,30 @@ $( document ).ready(function() {
 	    	leg.fromName = stationData.stationList[i].name;
 	    	leg.toName = stationData.stationList[i-1].name;
 	    	leg.duration = durData.durations[i-1].duration;
-	    	legList.push(leg);
-	    	console.log( leg );
+	    	legList.push(leg);	    	
 	    }
-	    console.log( legList.length );
 	}
+
+	addTrip(legList);
+		
+	
+    
+});
+
+function addTrip(legList)
+{
+	if(legList.length<=6)
+	{
+		addFullTrip(legList);		
+	}
+	else
+	{
+		addPartialTrip(legList);
+	}
+}
+
+function addFullTrip(legList)
+{
 	var totalTripDuration = 0;
 	for(i=0;i<legList.length;i++)
 	{	
@@ -57,11 +77,50 @@ $( document ).ready(function() {
 		}
 		totalTripDuration+=legList[i].duration;
 	}
-	
-	//$(".routeInfo").append("<span>"+"Durata calatoriei: "+'</span>');
+
 	$(".routeInfo").append("<span>"+"Durata călătoriei: "+totalTripDuration+" min"+'</span>');
-    
-});
+}
+
+function addPartialTrip(legList)
+{
+	if(legList.length<=6)
+	{
+		addFullTrip(legList);
+		return;
+	}
+
+	var totalTripDuration = 0;
+	var sectionedTripDuration = 0;
+	for(i=0;i<legList.length;i++)
+	{	
+		if(i==0)
+		{
+			addFirstLi(i,legList);
+		}
+		else
+		{
+			if(i==1 || i==2 || i== legList.length -2 || i==  legList.length -1)
+			{
+				addGreenLi(i,legList);
+			}
+			else
+			{
+				sectionedTripDuration+=legList[i].duration;
+				if (i== legList.length -3)
+				{
+					addMoreLi(legList.length-5,sectionedTripDuration);
+				}
+			}
+		}
+		if(i == legList.length -1 )
+		{
+			addEndLi(i,legList);
+		}
+		totalTripDuration+=legList[i].duration;
+	}
+	
+	$(".routeInfo").append("<span>"+"Durata călătoriei: "+totalTripDuration+" min"+'</span>');
+}
 
 function addFirstLi(i,legList)
 {
@@ -84,6 +143,14 @@ function addEndLi(i,legList)
 	var currentStub = locationStub.replace("[color]","red");
 			currentStub = currentStub.replace("[StationName]",legList[i].toName);
 			currentStub = currentStub.replace("[Duration]","");
+			$(".experiences").append(currentStub);
+}
+
+function addMoreLi(numberOfStations, totalDuration)
+{
+	var currentStub = locationStub.replace("[color]","details");
+			currentStub = currentStub.replace("[StationName]","Alte "+numberOfStations+" staţii");
+			currentStub = currentStub.replace("[Duration]",totalDuration+" min");
 			$(".experiences").append(currentStub);
 }
 
